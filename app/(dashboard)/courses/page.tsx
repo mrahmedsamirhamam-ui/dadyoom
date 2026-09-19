aacf152cf993dad976aac0f116e3360325a054a9
MrAhmedsamir <mrahmedsamirhamam@gmail.com>
2026-09-19 21:17:55 +0300

    move: remove protected course route (page.tsx)

--- app/(dashboard)/courses/page.tsx
+++
@@ -1,6 +0,0 @@
-import CurriculumCatalogClient from "./CurriculumCatalogClient";
-import { getStudentCurriculumCatalog } from "@/services/lessons/student-curriculum-catalog";
-
-export default async function CoursesPage() {
-  return <CurriculumCatalogClient units={await getStudentCurriculumCatalog()} />;
-}
