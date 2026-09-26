64c8419ab81a3b423a804373b7670daddcd520de
MrAhmedsamir <mrahmedsamirhamam@gmail.com>
2026-09-26 19:03:00 +0300

    perf: statically render signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,8 @@
 import EmailPasswordAuthForm from "@/components/auth/EmailPasswordAuthForm";
 
+export const dynamic = "force-static";
+export const revalidate = 86400;
+
 export default function SignupPage() {
   return <EmailPasswordAuthForm mode="signup" />;
 }
